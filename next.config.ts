f04c6a35a7c1bed6b98076f88e58868b5ac46179
yeboong99 <143702092+yeboong99@users.noreply.github.com>
2025-02-17 21:14:00 +0900

    feat: nextconfig 헤더설정 추가

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,30 @@ const nextConfig: NextConfig = {
       },
     ];
   },
+
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          { key: 'Access-Control-Allow-Origin', value: 'https://toleave.shop' },
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET, POST, OPTIONS, DELETE, PUT',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'Authorization, Content-Type, Refresh-Token',
+          },
+          {
+            key: 'Access-Control-Expose-Headers',
+            value: 'Authorization, Refresh-Token',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
