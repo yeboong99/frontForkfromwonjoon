125e3584008d0e7b55a1c236ef0f86d7840834dd
yeboong99 <143702092+yeboong99@users.noreply.github.com>
2025-02-20 18:38:10 +0900

    feat: nextconfigts 수정

--- next.config.ts
+++ next.config.ts
@@ -1,24 +1,28 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async rewrites() {
+    return [];
+  },
+
   async headers() {
     return [
       {
-        source: '/(.*)',
+        source: "/(.*)",
         headers: [
-          { key: 'Access-Control-Allow-Origin', value: 'https://toleave.shop' },
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: "Access-Control-Allow-Origin", value: "https://toleave.shop" },
+          { key: "Access-Control-Allow-Credentials", value: "true" },
           {
-            key: 'Access-Control-Allow-Methods',
-            value: 'GET, POST, OPTIONS, DELETE, PUT',
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, OPTIONS, DELETE, PUT",
           },
           {
-            key: 'Access-Control-Allow-Headers',
-            value: 'Authorization, Content-Type, Refresh-Token',
+            key: "Access-Control-Allow-Headers",
+            value: "Authorization, Content-Type, Refresh-Token",
           },
           {
-            key: 'Access-Control-Expose-Headers',
-            value: 'Authorization, Refresh-Token',
+            key: "Access-Control-Expose-Headers",
+            value: "Authorization, Refresh-Token",
           },
         ],
       },
